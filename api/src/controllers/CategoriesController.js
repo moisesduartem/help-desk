e5ac29e3429f4d1fdb82c40c845168942f54e3b4
Moisés Mariano <moisesduartem@gmail.com>
2021-03-07 11:36:12 -0300

    feat: create a route to list all categories

---
+++ api/src/controllers/CategoriesController.js
@@ -0,0 +1,17 @@
+import { roles } from '../enum/roles';
+import Category from '../models/category';
+
+class CategoriesController {
+
+    async index(req, res) {
+        try {
+            const categories = await Category.find();
+            return res.json({ categories });
+        } catch(err) {
+            return res.status(500).send(err);
+        }
+
+    }
+}
+
+export default new CategoriesController();
--- api/src/routes.js
+++ api/src/routes.js
@@ -3,6 +3,7 @@ import { checkCredentials, unauthorizeCostumers } from './middlewares/auth';
 import UserController from './controllers/UserController';
 import AuthController from './controllers/AuthController';
 import SeedController from './controllers/SeedController';
+import CategoriesController from './controllers/CategoriesController';
 
 const routes = new Router();
 
@@ -11,6 +12,8 @@ routes.get('/users/logged', checkCredentials, UserController.logged);
 routes.get('/users/roles', checkCredentials, UserController.roles);
 routes.get('/users', checkCredentials, unauthorizeCostumers, UserController.index);
 routes.post('/users/register', checkCredentials, unauthorizeCostumers, UserController.store);
+
+routes.get('/categories', checkCredentials, CategoriesController.index);
 
 routes.post('/seed', SeedController.run);
 
