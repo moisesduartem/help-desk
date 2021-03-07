b63193e6d186c30beb4530c1c5a066dd5935e7ee
Moisés Mariano <moisesduartem@gmail.com>
2021-03-07 11:06:14 -0300

    fix: Lider -> Líder Técnico

--- api/src/enum/roles.js
+++ api/src/enum/roles.js
@@ -1,7 +1,7 @@
 export const roles = {
     COSTUMER: 'Cliente', 
     TECHNICIAN: 'Técnico', 
-    TECH_LEADER: 'Lider Técnico', 
+    TECH_LEADER: 'Líder Técnico', 
     SYSTEMS_ANALYST: 'Analista de Sistemas',
     NETWORK_ANALYST: 'Analista de Redes',
 };
