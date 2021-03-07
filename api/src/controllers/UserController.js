0181f908639f1bd9a4537c93193021414988cd65
Moisés Mariano <moisesduartem@gmail.com>
2021-03-07 09:43:09 -0300

    fix: return users in a object key named 'users'

--- api/src/controllers/UserController.js
+++ api/src/controllers/UserController.js
@@ -1,17 +1,17 @@
 import User from '../schema';
 
 class UserController {
-    
+
     async index(req, res) {
         const users = await User.find();
-        return res.json(users || []);
+        return res.json({ users });
     }
 
     async store(req, res) {
 
         const { name, email, password } = req.body;
         const user = await User.create({ name, email, password });
-        
+
         return res.json({ user: user.show() });
     }
 }
