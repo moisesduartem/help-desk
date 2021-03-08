5cdec38090cf6865f394363a24c5006bcf39ebd3
Moisés Mariano <moisesduartem@gmail.com>
2021-03-07 22:06:51 -0300

    feat: create a login to show or not the password with a checkbox

--- web/src/pages/Login/index.js
+++ web/src/pages/Login/index.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Checkbox, Column, Input } from '../../styles';
 import { AppName, FormControl, PageContainer } from './styles';
 
+
+function useShowPassword(initialValue = false) {
+    
+    const [mustShowPassword, setMustShowPassword] = useState(initialValue);
+    const [passwordInputType, setPasswordInputType] = useState("password");
+
+    useEffect(() => {
+        setPasswordInputType(mustShowPassword ? "text" : "password");
+    }, [mustShowPassword]);
+
+    const handleShowPassword = e => setMustShowPassword(!mustShowPassword);
+
+    return { passwordInputType, handleShowPassword };
+}
+
 function Login(props) {
+
+    const initialCheckboxValue = false;
+    const { passwordInputType, handleShowPassword } = useShowPassword(initialCheckboxValue);
+
     return (
         <PageContainer>
             <Column>
@@ -11,9 +30,9 @@ function Login(props) {
                     <label>Email</label>
                     <Input type="email" name="email" required />
                     <label>Senha</label>
-                    <Input type={"password"} name="password" required />
+                    <Input type={passwordInputType} name="password" required />
                     <FormControl>
-                        <Checkbox id="showPassword" />
+                        <Checkbox id="showPassword" defaultChecked={initialCheckboxValue} onChange={handleShowPassword} />
                         <label>Mostrar senha?</label>
                     </FormControl>
                     <Button>Entrar</Button>
