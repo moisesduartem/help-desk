a1ba52889e1a4ba7103bb060311b62d233256b62
Moisés Mariano <moisesduartem@gmail.com>
2021-03-15 22:32:39 -0300

    other pages will redirect user to /login on auth routes

--- web/src/routes/auth.js
+++ web/src/routes/auth.js
@@ -6,14 +6,11 @@ function AuthRoutes() {
 
     return (
         <Fragment>
-            <Route exact path="/">
+            <Route exact path="*">
                 <Redirect to="/login" />
             </Route>
             <Route exact path="/login">
                 <Login />
-            </Route>
-            <Route path="*">
-                <h1>404</h1>
             </Route>
         </Fragment>
     );
