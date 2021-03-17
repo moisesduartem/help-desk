deb5e16fef48860811a95c1b15ac987aefd94121
Moisés Mariano <moisesduartem@gmail.com>
2021-03-16 21:45:00 -0300

    feat: dinamically shows the user name

--- web/src/components/Header/index.js
+++ web/src/components/Header/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Container } from './styles';
+import { useAuth } from '../../context/auth';
 
 function Header() {
-    
+
+    const { user } = useAuth();
+
     return (
         <Container>
-            <span>Moisés Mariano</span>
+            <span>{user.name}</span>
             <Link to="/">App Help Desk</Link>
             <Link to="/logout">Sair</Link>
         </Container>
